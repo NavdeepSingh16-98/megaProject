7e28da6109ea12bac8ed90b99282230d3c2068a0
NavdeepSingh16-98 <seraph.navdeep@gmail.com>
2023-01-19 14:21:42 +0530

    methods in userSchema and compare pwd

--- models/user.schema.js
+++ models/user.schema.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 import AuthRoles from '../utils/authRoles'
 import bcrypt from 'bcryptjs'
 import JWT from 'jsonwebtoken'
-import crypto from 'crypto'
+import crypto from 'crypto',
+import config from "../config/config";
 
 const userSchema = mongoose.Schema(
     {
@@ -45,6 +46,31 @@ userSchema.pre("save",async function(next){
     }
     next()
 })
+
+// add more features directly to your schema
+
+userSchema.methods = {
+
+    //compare password
+    comparePassword:async function(enteredPassword){
+
+        return await bcrypt.compare(enteredPassword,this.password)
+    },
+
+    //generate JWT token
+
+    getJwtToken:function(){
+return JWT.sign({
+    _id:this._id,
+    role:this.role
+},
+config.JWT_SECRET,
+{
+    expiresIn:config.JWT_EXPIRY
+}
+)
+    }
+}
 
 export default mongoose.model("User",userSchema)
 
