1b6e6310db59c9a10ed4fe1fdaea45a6452ae352
NavdeepSingh16-98 <seraph.navdeep@gmail.com>
2023-01-19 12:39:44 +0530

    mega project started

---
+++ utils/authRoles.js
@@ -0,0 +1,11 @@
+const AuthRoles = {
+    ADMIN:"ADMIN",
+    MODERATOR:"MODERATOR",
+    USER:"USER"
+}
+
+export default AuthRoles;
+
+// we are creating enums
+
+//we will always refer to it as admin
