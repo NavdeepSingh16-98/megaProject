64966e76a031f3f5dd23cf176a8f0f7bde0fa6f9
NavdeepSingh16-98 <seraph.navdeep@gmail.com>
2023-02-13 13:44:11 +0530

    AuthController - login, logout

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -54,3 +54,72 @@ console.log(user);
 
 
 })
+
+/********************************* 
+
+* @LOGIN 
+* @route http://localhost:4000/api/auth/login
+* @description User signIn controller for loging  new user
+* @parameters email,password
+* @return User Object
+
+************************************/
+
+export const login = asyncHandler(async(req,res)=>{
+
+    const {email,password} = req.body;
+
+    if( !email || !password){
+
+        throw new CustomError('Please fill all fields',400)
+    }
+    // check if user exists
+    const user = User.findOne({email}).select('+password');
+
+    if(!user){
+        throw new CustomError('Invalid credentials',400);
+    }
+
+   const isPasswordMatched =  await user.comparePassword(password);
+// we write invalid credetianls not email invalid to confuse attacker
+
+if(isPasswordMatched){
+const token = user.getJwtToken();
+user.password = undefined;
+res.cookie("token",token,cookieOptions);
+
+return res.status(200).json({
+    success:true,
+    token,
+    user
+})
+}
+
+throw new CustomError('Invalid credentials - pass',400);
+  
+
+})
+
+/********************************* 
+
+* @LOGOUT
+* @route http://localhost:4000/api/auth/logout
+* @description User logout by clearing user cookies
+* @parameters 
+* @return success message
+
+************************************/
+
+// '_req' is only for naming
+
+export const logout = asyncHandler(async (_req,res)=>{
+// below we are clearing the cookie named 'token'
+    res.cookie("token",null,{
+        expires:new Date(Date.now()),
+        httpOnly:true
+    })
+    res.status(200).json({
+        success:true,
+        message:"Logged Out"
+    })
+})
