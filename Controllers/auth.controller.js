305ed4c93323a56f822cdd40578c5eac913c8a48
NavdeepSingh16-98 <seraph.navdeep@gmail.com>
2023-02-13 13:17:04 +0530

    Authcontroller - SignUp

---
+++ Controllers/auth.controller.js
@@ -0,0 +1,56 @@
+import User from '../models/user.schema';
+import asyncHandler from '../services/asyncHandler';
+import CustomError from '../utils/customError';
+
+export const cookieOptions = {
+    expires:new Date(Date.now()+3*24*60*60*1000),
+    httpOnly:true
+    // could be in a separate file in utils
+}
+
+/********************************* 
+
+* @SIGNUP 
+* @route http://localhost:4000/api/auth/signup
+* @description User signup controller for creating a new user
+* @parameters name,email,password
+* @return User Object
+
+************************************/
+
+export const signUp = asyncHandler(async(req,res)=>{
+
+    const {name,email,password} = req.body;
+
+    if(!name || !email || !password){
+
+        throw new CustomError('Please fill all fields',400)
+    }
+    // check if user exists
+    const existingUser = User.findOne({email});
+
+    if(existingUser){
+        throw new CustomError('user already exists',400);
+    }
+
+    const  user = await User.create({
+        name,
+        email,
+        password
+    });
+
+    const token  = user.getJwtToken();
+console.log(user);
+// select false will only work while querying
+    user.password = undefined;
+
+    res.cookie('token',token,cookieOptions);
+
+    res.status(200).json({
+        success:true,
+        token,
+        user
+    })
+
+
+})
