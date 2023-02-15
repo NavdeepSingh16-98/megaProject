a4b00852ac2e56c5c5b329226a0cf06aa53e4084
Navdeep Singh <seraph.navdeep@gmail.com>
2023-02-15 07:24:26 +0000

    created a mail utility

--- config/config.js
+++ config/config.js
@@ -6,7 +6,12 @@ const config = {
     JWT_SECRET:process.env.JWT_SECRET,
     JWT_EXPIRY:process.env.JWT_EXPIRY || '30d',
     MONGODB_URL:process.env.MONGODB_URL,
-    PORT:process.env.PORT
+    PORT:process.env.PORT,
+    SMTP_MAIL_HOST:process.env.HOST,
+SMTP_MAIL_PORT:process.env.PORT,
+SMTP_MAIL_USERNAME:process.env.USERNAME,
+SMTP_MAIL_PASSWORD:process.env.PASSWORD,
+SMTP_MAIL_EMAIL:process.env.EMAIL
 }
 
 export default config
