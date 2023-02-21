53af6887063d537b0066e69bce22751aa3244cd9
Navdeep Singh <seraph.navdeep@gmail.com>
2023-02-21 16:36:09 +0000

    added middleware for loggedIn

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,42 @@
+import User from '../models/user.schema.js'
+
+import JWT from 'jsonwebtoken'
+
+import asyncHandler from '../services/asyncHandler';
+import CustomError from '../utils/customError';
+
+import config from '../config/config.js';
+import { request } from 'express';
+
+
+export const isLoggedIn = asyncHandler(async(req,res,next)=>{
+
+    let token;
+
+    if(req.cookies.token || 
+        (req.headers.authorization && req.headers.authorization.startsWith("Bearer"))
+        ){
+
+
+            token = req.cookies.token || req.headers.authorization.split(" ")[1];
+
+        }
+
+        if(!token){
+            throw new CustomError('Not authorized to access this route',401);
+        }
+
+        try{
+
+          const decodedJwtPayload =  JWT.verify(token,config.JWT_SECRET);
+
+        req.user = await  User.findById(decodedJwtPayload._id,"name email role")
+
+next();
+
+        }catch(error){
+            throw new CustomError('Not authorized to access this route',401);
+
+
+        }
+})
